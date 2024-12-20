67172298e88e98da51adc8ddabc587a2b99d6846
juanmatoniolo <juanmatoniolo2@gmail.com>
2024-12-20 15:23:16 -0300

    Updates

---
+++ precache-manifest.70927307426031b9adb4dd15794c2335.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60cc4b31335d4e44cd7812e0ba60eb16",
+    "url": "/HANAKOTOBA/index.html"
+  },
+  {
+    "revision": "16676a6798930b005ecf",
+    "url": "/HANAKOTOBA/static/css/2.dbb74888.chunk.css"
+  },
+  {
+    "revision": "4e873d3b42294b423236",
+    "url": "/HANAKOTOBA/static/css/main.ce05523f.chunk.css"
+  },
+  {
+    "revision": "16676a6798930b005ecf",
+    "url": "/HANAKOTOBA/static/js/2.940603a3.chunk.js"
+  },
+  {
+    "revision": "4e873d3b42294b423236",
+    "url": "/HANAKOTOBA/static/js/main.dd6167c0.chunk.js"
+  },
+  {
+    "revision": "baaa3e4164f1b88a89ef",
+    "url": "/HANAKOTOBA/static/js/runtime~main.301c2902.js"
+  },
+  {
+    "revision": "49451be2bf7fa1f45a45e707efaeae7d",
+    "url": "/HANAKOTOBA/static/media/depto1.49451be2.webp"
+  },
+  {
+    "revision": "f177217a03457d506e613017aaad8925",
+    "url": "/HANAKOTOBA/static/media/depto1.f177217a.jpg"
+  },
+  {
+    "revision": "962254983e0bf4d2a9f3cb16da991f7c",
+    "url": "/HANAKOTOBA/static/media/deptos (2).96225498.svg"
+  },
+  {
+    "revision": "35394eb90be8d26ce534b5bc06c82423",
+    "url": "/HANAKOTOBA/static/media/deptos (3).35394eb9.svg"
+  },
+  {
+    "revision": "4cd03d6979d65ce2e8e9aabd8dad9c66",
+    "url": "/HANAKOTOBA/static/media/deptos (4).4cd03d69.svg"
+  },
+  {
+    "revision": "c2249cff93a93849866eb6005273f24f",
+    "url": "/HANAKOTOBA/static/media/hanakotoba.svg.c2249cff.svg"
+  },
+  {
+    "revision": "91708c8c240766c5fb221434608d62e3",
+    "url": "/HANAKOTOBA/static/media/img-fondo-gris.91708c8c.png"
+  }
+]);
